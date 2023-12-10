e7c050772d4f18bbedacf180dd54fc9841148760
Merdasa Belay <merdasabelay@gmail.com>
2023-12-10 10:37:09 +0300

    setting whatsapp clone project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCS0uUXQ0CfaV5qnC8Xxs2D9qFJikYza94",
+  authDomain: "whatsapp-clone-5a8aa.firebaseapp.com",
+  projectId: "whatsapp-clone-5a8aa",
+  storageBucket: "whatsapp-clone-5a8aa.appspot.com",
+  messagingSenderId: "985750254696",
+  appId: "1:985750254696:web:79cbd3b16e2d46de21b7dd",
+  measurementId: "G-FGX04V4788",
+};
